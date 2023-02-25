6f5634c60ce7057063e69aea80a7687e53a62614
developer <gruj1995@berich.net.tw>
2023-02-25 09:34:17 +0800

    feat: 在AppDelegate設置statusBarStyle、UIBarButtonItem的全域tintColor、NavigationBar全域appearance、導入Flex debug套件、修正iOS 15 後tableView section上方多出空白的問題

--- ITunesPractice/AppDelegate.swift
+++ ITunesPractice/AppDelegate.swift
@@ -6,11 +6,39 @@
 //
 
 import UIKit
+#if DEBUG
+import FLEX
+#endif
+
+// MARK: - AppDelegate
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+#if DEBUG
+        FLEXManager.shared.isNetworkDebuggingEnabled = true
+#endif
+        return true
+    }
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // 要先到 info.plist 新增 key(View controller-based status bar appearance) 以下設置才有效
+        UIApplication.shared.statusBarStyle = .lightContent
+        
+        // 設置所有 UIBarButtonItem 的 tinitColor
+        UIBarButtonItem.appearance().tintColor = .appColor(.red1)
+        
+        setNavigationBarAppearance()
+        
+        // 修正ios 15 tableView section 上方多出的空白
+        if #available(iOS 15.0, *) {
+            UITableView.appearance().sectionHeaderTopPadding = 0.0
+        }
+        
+#if DEBUG
+        FLEXManager.shared.isNetworkDebuggingEnabled = true
+#endif
         return true
     }
 
@@ -27,4 +55,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+}
+
+extension AppDelegate {
+    private func setNavigationBarAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+        //        appearance.configureWithOpaqueBackground() // 預設背景及陰影
+        //        appearance.configureWithTransparentBackground() // 透明背景且無陰影(隱藏底部邊框)
+        // 模糊效果樣式(可調亮/暗程度)
+        appearance.backgroundEffect = UIBlurEffect(style: .systemMaterialDark)
+        appearance.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .semibold)
+        ]
+        appearance.largeTitleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white
+        ]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+}
+
+extension UIWindow {
+    #if DEBUG
+    /// 搖動出現debug套件
+    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionEnded(motion, with: event)
+        if motion == .motionShake {
+            FLEXManager.shared.showExplorer()
+        }
+    }
+    #endif
 }
