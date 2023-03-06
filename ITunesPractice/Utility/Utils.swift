d97c1c7474c00b0ad098558456f5452977449878
developer <gruj1995@berich.net.tw>
2023-03-07 01:14:27 +0800

    feat: 新增 Utils 檔案，並新增查找App根目錄路徑的funtion

---
+++ ITunesPractice/Utility/Utils.swift
@@ -0,0 +1,15 @@
+//
+//  Utils.swift
+//  ITunesPractice
+//
+//  Created by 李品毅 on 2023/3/6.
+//
+
+import Foundation
+
+struct Utils {
+    /// 獲取App的根目錄路徑
+    static func applicationSupportDirectoryPath() -> String {
+        NSHomeDirectory()
+    }
+}
