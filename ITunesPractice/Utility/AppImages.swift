3cc19c93029357dfcede0286dd186b7371a36b79
developer <gruj1995@berich.net.tw>
2023-03-13 14:49:53 +0800

    feat: AppImages中放入讓SF Symbols向量圖變大的設置範例，並新增部分圖片常數

--- ITunesPractice/Utility/AppImages.swift
+++ ITunesPractice/Utility/AppImages.swift
@@ -7,14 +7,23 @@
 
 import UIKit
 
+// 因為 SF Symbols 是向量圖，所以要放大icon的話要透過SymbolConfiguration設置
+let playerButtonConfiguration = UIImage.SymbolConfiguration(pointSize: 20, weight: .regular, scale: .large)
+
 enum AppImages {
     // 放大境
-    static let magnifyingGlass = UIImage(systemName: "magnifyingglass") 
+    static let magnifyingGlass = UIImage(systemName: "magnifyingglass")
+
+    // tabbar
     static let musicHouse = UIImage(systemName: "music.note.house.fill")
-    // 暫停
+
+    // 播放器
     static let pause = UIImage(systemName: "pause.fill")
-    // 播放
     static let play = UIImage(systemName: "play.fill")
+    static let forward = UIImage(systemName: "forward.fill")
+    static let pauseLarge = UIImage(systemName: "pause.fill", withConfiguration: playerButtonConfiguration)
+    static let playLarge = UIImage(systemName: "play.fill", withConfiguration: playerButtonConfiguration)
+    static let forwardLarge = UIImage(systemName: "forward.fill", withConfiguration: playerButtonConfiguration)
 
     // context menu
     static let plus = UIImage(systemName: "plus")
@@ -26,5 +35,8 @@ enum AppImages {
     // PreviewType
     static let person = UIImage(systemName: "person.fill")
     static let musicList = UIImage(systemName: "music.note.list")
+
+    // 透過 pinterest 找圖和 figma 修改尺寸與透明度產生
+    static let musicNote = UIImage(named: "music.note")
 }
 
