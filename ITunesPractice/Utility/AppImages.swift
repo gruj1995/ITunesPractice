287e2fbe2106fdd0aa4517ba624b430cafa52be9
developer <gruj1995@berich.net.tw>
2023-03-15 20:15:21 +0800

    style: 說明為何AppImages使用enum 搭配 static let 參數

--- ITunesPractice/Utility/AppImages.swift
+++ ITunesPractice/Utility/AppImages.swift
@@ -10,6 +10,11 @@ import UIKit
 // 因為 SF Symbols 是向量圖，所以要放大icon的話要透過SymbolConfiguration設置
 let playerButtonConfiguration = UIImage.SymbolConfiguration(pointSize: 20, weight: .regular, scale: .large)
 
+/// 使用 enum 搭配 static let 有兩個原因:
+/// 1. 因為無法初始化沒有case的enum，更能表達常數的概念
+/// 2. 將相關的常數放在 enum 中可能感覺更自然，因為 enum 用於儲存一組相關值
+/// https://forums.swift.org/t/static-let-in-enum-vs-struct/36152/12
+
 enum AppImages {
     // 放大境
     static let magnifyingGlass = UIImage(systemName: "magnifyingglass")
@@ -36,7 +41,7 @@ enum AppImages {
     static let person = UIImage(systemName: "person.fill")
     static let musicList = UIImage(systemName: "music.note.list")
 
-    // 透過 pinterest 找圖和 figma 修改尺寸與透明度產生
+    // 這張圖透過 pinterest 找圖和 figma 修改尺寸與透明度做出來的～
     static let musicNote = UIImage(named: "music.note")
 }
 
