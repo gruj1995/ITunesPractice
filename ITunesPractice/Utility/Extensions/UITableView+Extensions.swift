7a7cf9e9a5ac071f727b799eb7b7e91e825ce355
developer <gruj1995@berich.net.tw>
2023-03-20 22:23:51 +0800

    feat: UITableView+Extensions 檔案中新增 scrollToTop(_:) 和計算目前可見的section的相關方法

--- ITunesPractice/Utility/Extensions/UITableView+Extensions.swift
+++ ITunesPractice/Utility/Extensions/UITableView+Extensions.swift
@@ -32,4 +32,87 @@ extension UITableView {
         }
         return configuration.createContextMenuConfiguration()
     }
+
+    func scrollToTop(animated: Bool) {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            let indexPath = IndexPath(row: 0, section: 0)
+            if self.hasRowAtIndexPath(indexPath: indexPath) {
+                self.scrollToRow(at: indexPath, at: .top, animated: animated)
+            }
+        }
+    }
+
+    func hasRowAtIndexPath(indexPath: IndexPath) -> Bool {
+        return indexPath.section < self.numberOfSections && indexPath.row < self.numberOfRows(inSection: indexPath.section)
+    }
+
+    /// tableView 最後的 section 是否可見
+    func isLastSectionReached() -> Bool {
+        guard numberOfSections > 0 else {
+            return true
+        }
+
+        let lastSectionIndex = numberOfSections - 1
+        let numberOfRowsInLastSection = numberOfRows(inSection: lastSectionIndex)
+
+        guard numberOfRowsInLastSection > 0 else {
+            var sectionIndex = lastSectionIndex - 1
+
+            while sectionIndex >= 0 {
+                let numberOfRows = numberOfRows(inSection: sectionIndex)
+
+                if numberOfRows > 0 {
+                    return false
+                }
+
+                sectionIndex -= 1
+            }
+
+            return true
+        }
+
+        let lastRowIndex = numberOfRowsInLastSection - 1
+        let lastIndexPath = IndexPath(row: lastRowIndex, section: lastSectionIndex)
+        let lastVisibleIndexPaths = indexPathsForVisibleRows ?? []
+
+        return lastVisibleIndexPaths.contains(lastIndexPath)
+    }
+
+    // github文件 https://stackoverflow.com/questions/15204328/how-to-retrieve-all-visible-table-section-header-views/23538021#23538021
+
+    var indexesOfVisibleSections: [Int] {
+        // Note: We can't just use indexPathsForVisibleRows, since it won't return index paths for empty sections.
+        var visibleSectionIndexes = [Int]()
+
+        for i in 0 ..< numberOfSections {
+            var headerRect: CGRect?
+            // In plain style, the section headers are floating on the top, so the section header is visible if any part of the section's rect is still visible.
+            // In grouped style, the section headers are not floating, so the section header is only visible if it's actualy rect is visible.
+            if style == .plain {
+                headerRect = rect(forSection: i)
+            } else {
+                headerRect = rectForHeader(inSection: i)
+            }
+            if headerRect != nil {
+                // The "visible part" of the tableView is based on the content offset and the tableView's size.
+                let visiblePartOfTableView = CGRect(x: contentOffset.x, y: contentOffset.y, width: bounds.size.width, height: bounds.size.height)
+                if visiblePartOfTableView.intersects(headerRect!) {
+                    visibleSectionIndexes.append(i)
+                }
+            }
+        }
+        return visibleSectionIndexes
+    }
+
+    var visibleSectionHeaders: [UITableViewHeaderFooterView] {
+        var visibleSects = [UITableViewHeaderFooterView]()
+        for sectionIndex in indexesOfVisibleSections {
+            if let sectionHeader = headerView(forSection: sectionIndex) {
+                visibleSects.append(sectionHeader)
+            }
+        }
+
+        return visibleSects
+    }
 }
