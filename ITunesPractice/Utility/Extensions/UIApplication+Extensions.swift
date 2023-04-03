a33fb3c9cc47fe0836e98bb0c7b30b2f9f671c91
developer <gruj1995@berich.net.tw>
2023-04-03 11:16:30 +0800

    feat: 調整key window的取法

--- ITunesPractice/Utility/Extensions/UIApplication+Extensions.swift
+++ ITunesPractice/Utility/Extensions/UIApplication+Extensions.swift
@@ -10,10 +10,17 @@ import UIKit
 extension UIApplication {
     var keyWindowCompact: UIWindow? {
         if #available(iOS 13.0, *) {
-            return UIApplication.shared.connectedScenes
-                .filter { $0.activationState == .foregroundActive }
-                .compactMap { $0 as? UIWindowScene }.first?.windows
-                .first(where: \.isKeyWindow)
+            guard let keyWindow = UIApplication.shared.windows.first(where: \.isKeyWindow) else {
+                for scene in UIApplication.shared.connectedScenes {
+                    if let windowScene = scene as? UIWindowScene {
+                        for window in windowScene.windows where window.isKeyWindow {
+                            return window
+                        }
+                    }
+                }
+                return nil
+            }
+            return keyWindow
         } else {
             return UIApplication.shared.keyWindow
         }
